b650edf6816ef5929681c8accf6e09550d4a7929
dn270592ten1 <evgenij.terescenko@privatbank.ua>
2021-08-23 20:54:25 +0300

    add .gitignore

--- dag/dimensions_dag.py
+++ dag/dimensions_dag.py
@@ -15,44 +15,37 @@ pg_creds = {
     "database": "dshop_bu"
 }
 
-def upload_df(df_name):
+dimension_dfs = [
+    'aisles',
+    'clients',
+    'departments',
+    'location_areas',
+    'products',
+    'store_types',
+    'stores'
+]
+
+fact_df = 'orders'
+
+def upload_dimension_dfs():
+    client = InsecureClient(hdfs_url, user="user")
+    date = datetime.today().date()
+
+    for df in dimension_dfs:
+        with psycopg2.connect(**pg_creds) as pg_connection:
+            cursor = pg_connection.cursor()
+            with client.write(f'/bronze/{str(date)}/{df}.csv') as csv_file:
+                cursor.copy_expert(f"COPY {df} TO STDOUT WITH HEADER CSV", csv_file)
+
+
+def upload_fact_df():
     client = InsecureClient(hdfs_url, user="user")
     date = datetime.today().date()
 
     with psycopg2.connect(**pg_creds) as pg_connection:
         cursor = pg_connection.cursor()
-        with client.write(f'/bronze/{str(date)}/{df_name}.csv') as csv_file:
-            cursor.copy_expert(f"COPY {df_name} TO STDOUT WITH HEADER CSV", csv_file)
-
-
-def upload_aisles():
-    df_name = 'aisles'
-    upload_df(df_name)
-
-def upload_clients():
-    df_name = 'clients'
-    upload_df(df_name)
-
-def upload_departments():
-    df_name = 'departments'
-    upload_df(df_name)
-
-def upload_location_areas():
-    df_name = 'location_areas'
-    upload_df(df_name)
-
-def upload_products():
-    df_name = 'products'
-    upload_df(df_name)
-
-def upload_store_types():
-    df_name = 'store_types'
-    upload_df(df_name)
-
-def upload_stores():
-    df_name = 'stores'
-    upload_df(df_name)
-
+        with client.write(f'/bronze/{str(date)}/{fact_df}.csv') as csv_file:
+            cursor.copy_expert(f"COPY {fact_df} TO STDOUT WITH HEADER CSV", csv_file)
 
 dag = DAG(
     dag_id="dimensions",
@@ -63,50 +56,25 @@ dag = DAG(
 )
 
 t1 = PythonOperator(
-    task_id="upload aisles",
+    task_id="upload dimension dfs to bronze",
     dag=dag,
-    python_callable=upload_aisles,
+    python_callable=upload_dimension_dfs,
     provide_context=True
 )
 
 t2 = PythonOperator(
-    task_id="upload clients",
+    task_id="upload orders df to bronze",
     dag=dag,
-    python_callable=upload_clients,
+    python_callable=upload_fact_df,
     provide_context=True
 )
 
 t3 = PythonOperator(
-    task_id="upload departments",
-    dag=dag,
-    python_callable=upload_departments,
-    provide_context=True
+    task_id="silver preparation"
+
 )
 
 t4 = PythonOperator(
-    task_id="upload location_areas",
-    dag=dag,
-    python_callable=upload_location_areas,
-    provide_context=True
+
 )
 
-t5 = PythonOperator(
-    task_id="upload products",
-    dag=dag,
-    python_callable=upload_products,
-    provide_context=True
-)
-
-t6 = PythonOperator(
-    task_id="upload store_types",
-    dag=dag,
-    python_callable=upload_store_types,
-    provide_context=True
-)
-
-t7 = PythonOperator(
-    task_id="upload stores",
-    dag=dag,
-    python_callable=upload_stores,
-    provide_context=True
-)
